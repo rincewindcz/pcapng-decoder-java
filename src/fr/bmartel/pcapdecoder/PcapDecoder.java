6aa23729f741abefc97402b6b4f08e8c7643e6a1
Michal Genserek <michal@fortes.cz>
2016-04-17 22:34:25 +0200

    added myself into authors

--- src/fr/bmartel/pcapdecoder/PcapDecoder.java
+++ src/fr/bmartel/pcapdecoder/PcapDecoder.java
@@ -2,6 +2,7 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2015 Bertrand Martel
+ * Copyright (c) 2016 Michal Genserek
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -41,6 +42,7 @@ import java.util.logging.Logger;
  * PCAP NG decoder
  *
  * @author Bertrand Martel
+ * @author Michal Genserek
  *
  */
 public class PcapDecoder {
